cab8c51316938fd9a0162ea8b3820a9798fe7a2f
guilherme.veloso <guilherme.veloso@CONTW-DES79.CONTMATIC.AD>
2019-09-02 15:59:44 -0300

    Documentada a classe funcionario.

--- Prova03/src/main/java/br/com/contmatic/models/Funcionario.java
+++ Prova03/src/main/java/br/com/contmatic/models/Funcionario.java
@@ -5,15 +5,35 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Funcionario.
+ */
 public class Funcionario extends PessoaFisica {
+    
+    /** The salario. */
     private Short salario;
+    
+    /** The comissao. */
     private Short comissao;
+    
+    /** The departamento. */
     private String departamento;
 
+    /**
+     * Instantiates a new funcionario.
+     */
     public Funcionario() {
         super();
     }
 
+    /**
+     * Instantiates a new funcionario.
+     *
+     * @param salario the salario
+     * @param comissao the comissao
+     * @param departamento the departamento
+     */
     public Funcionario(Short salario, Short comissao, String departamento) {
         super();
         this.salario = salario;
@@ -21,6 +41,11 @@ public class Funcionario extends PessoaFisica {
         this.departamento = departamento;
     }
 
+    /**
+     * Gets the salario.
+     *
+     * @return the salario
+     */
     @NotNull(message = "Salário é obrigatório.")
     @DecimalMin(value = "1000.00", message = "O salário não pode ser menor que a base de R$: 1000.00.")
     @DecimalMax(value = "9999.99", message = "O salário não pode ser maior que R$: 9999.99.")
@@ -28,26 +53,51 @@ public class Funcionario extends PessoaFisica {
         return salario;
     }
 
+    /**
+     * Sets the salario.
+     *
+     * @param salario the new salario
+     */
     public void setSalario(Short salario) {
         this.salario = salario;
     }
 
+    /**
+     * Gets the comissao.
+     *
+     * @return the comissao
+     */
     @DecimalMin(value = "0.0", message = "A comissão não pode ser menor que R$: 0.00.")
     @DecimalMax(value = "500.00", message = "A comissão não pode ser maior que R$: 500.00.")
     public Short getComissao() {
         return comissao;
     }
 
+    /**
+     * Sets the comissao.
+     *
+     * @param comissao the new comissao
+     */
     public void setComissao(Short comissao) {
         this.comissao = comissao;
     }
 
+    /**
+     * Gets the departamento.
+     *
+     * @return the departamento
+     */
     @NotNull(message = "Ter um departamento é obrigatório.")
     @Size(min = 1, max = 30)
     public String getDepartamento() {
         return departamento;
     }
 
+    /**
+     * Sets the departamento.
+     *
+     * @param departamento the new departamento
+     */
     public void setDepartamento(String departamento) {
         this.departamento = departamento;
     }
